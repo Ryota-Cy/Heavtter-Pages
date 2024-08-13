947920cb9d0e3451b57a3e284e38962a6414f155
Ryota-Cy <Ryota.Sai.net@gmail.com>
2024-08-13 14:12:10 +0900

    remove distDir

--- heavtter-pages/next.config.mjs
+++ heavtter-pages/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'heavtter-pages/out',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/my-repo-name/' : '',
     images: {
       unoptimized: true,
