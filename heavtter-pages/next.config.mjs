b65b3be44d5d9babe691121b1ce5c1a822acacd5
Ryota-Cy <Ryota.Sai.net@gmail.com>
2024-08-13 16:15:40 +0900

    Set repoName

--- heavtter-pages/next.config.mjs
+++ heavtter-pages/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-repo-name/' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Heavtter.github.io/' : '',
     images: {
       unoptimized: true,
   },
