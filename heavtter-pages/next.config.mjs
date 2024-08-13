e9107aff0625be8b6216fc13a902090e98e5f05a
Ryota-Cy <Ryota.Sai.net@gmail.com>
2024-08-13 13:30:04 +0900

    Add distDir
    NextConfig Add distDir option.

--- heavtter-pages/next.config.mjs
+++ heavtter-pages/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    distDir: 'heavtter-pages/out',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/my-repo-name/' : '',
     images: {
       unoptimized: true,
